a6abd98c5894542d363a394c63d2b266a8eef7cd
jrlimar <jnr.lima@gmail.com>
2022-03-02 09:34:15 -0300

    summary html

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-import GetGrupoConfig from "./scenarios/get-grupo-config.js";
+import GetGrupoConfig from "./scenarios/get-config.js";
 import GetStatus from "./scenarios/get-status.js";
 import {group , sleep} from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+//Html Report
+export function handleSummary(data) {
+    return {
+      "summary.html": htmlReport(data),
+    };
+  }
 
 export default () => {
     group('API - ConfigController', () => {
